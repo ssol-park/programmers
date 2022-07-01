740ff9954c624893f21ad9ad4d422efb0bf043ad
ssol-park <ksttlov.naver@hanmail.net>
2022-07-01 19:52:28 +0900

    Feat: added Lv1, 'InnerProduct'

---
+++ src/main/java/lv1/InnerProduct.java
@@ -0,0 +1,19 @@
+package lv1;
+
+public class InnerProduct {
+
+    class Solution {
+        public int solution(int[] a, int[] b) {
+            int answer = 0;
+
+            for (int i = 0; i < a.length; i++) {
+
+                answer += a[i] * b[i];
+            }
+
+
+            return answer;
+        }
+    }
+
+}
