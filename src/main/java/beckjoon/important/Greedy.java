d9e00114bf3669484b4b60e48e1468df8cd0d51b
ssol-park <ksttlov0110@gmail.com>
2024-06-09 18:25:42 +0900

    Feat: Add beck1715, greedy algorithm

--- src/main/java/beckjoon/important/Greedy.java
+++ src/main/java/beckjoon/important/Greedy.java
@@ -3,22 +3,18 @@ package beckjoon.important;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
-/*
-* Beck11047
-* */
 public class Greedy {
 
-
     public static void main(String[] args) {
+
     }
 
+    /*
+     * 동전 개수의 최솟값 구하기
+     * 입력값 n = 10, k = 4200
+     * 코인가격 prices = {1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000
+     * */
     public static void beck11047() {
-        /*
-         * 동전 개수의 최솟값 구하기
-         * 입력값 n = 10, k = 4200
-         * 코인가격 prices = {1, 5, 10, 50, 100, 500, 1000, 5000, 10000, 50000
-         * */
-
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int k = scanner.nextInt();
@@ -38,5 +34,36 @@ public class Greedy {
         }
 
         System.out.println(coinCnt);
+    }
+
+    /*
+     * 카드 정렬하기
+     * 카드 묶음 수 n =3
+     * 카드 묶음 크기 cards = {10, 20, 40}
+     * */
+    public static void beck1715() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            queue.add(scanner.nextInt());
+        }
+
+        int sum = 0;
+        int firstBundle = 0;
+        int secBundle = 0;
+
+        while(queue.size() != 1) {
+            firstBundle = queue.remove();
+            secBundle = queue.remove();
+
+            sum += firstBundle + secBundle;
+
+            queue.add(sum);
+        }
+
+        System.out.println(sum);
     }
 }
